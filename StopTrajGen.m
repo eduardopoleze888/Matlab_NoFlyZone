29fc3cb0864206153c38f05c1f3ec3936577ed8e
eduardopoleze888 <eduardo.poleze@usp.br>
2017-08-31 15:47:34 -0300

    Geração de Trajetória funcionando

--- StopTrajGen.m
+++ StopTrajGen.m
@@ -1,31 +1,74 @@
-function [t1, J1, t2, J2, t3, J3, profile] = StopTrajGen(a0, v0, d, J, plot_traj)
+function [t1, J1, t2, J2, t3, J3, profile, delta_s] = StopTrajGen2(a0, v0, d, J, plot_traj)
 
 %% Description
 % Based on the initial acceleration (a0), velocity (v0), maximum
 % deceleration (d) and a desired Jerk (J), compute a triangular or
 % trapezoidal profile in order to stop a motion system.
 
-v_star = d^2/J - a0^2/(2*J);
-a02_2J_p = a0^2/(2*J);
+v_star = a0^2/(2*J) - d^2/(J);
+a0pow2_2J = a0^2/(2*J);
 
-if v0 <= v_star && v0 >= a02_2J_p
-    a_lim = -sqrt(J*v0 + a0^2/2);
-    t1 = -a_lim/J;
+% if v0 > -v_star
+%     t1 = (a0 + d)/J;
+%     J1 = -J;
+%     t2 = a0^2/(2*J*d) + a0/J + v0/d - t1;
+%     J2 = 0;
+%     t3 = d/J;
+%     J3 = +J;
+%     profile = 1;
+% elseif v0 > -a0pow2_2J
+%     a_lim = -sqrt(a0^2/2 + J*v0);
+%     t1 = (a0 - a_lim)/J;
+%     J1 = -J;
+%     t2 = -a_lim/J;
+%     J2 = J;
+%     t3 = 0;
+%     J3 = 0;
+%     profile = 0;
+if v0 < v_star
+    t1 = (d - a0)/J;
+    J1 = J;
+    t2 = a0^2/(2*J*d) - a0/J - v0/d - t1;
+    J2 = 0;
+    t3 = d/J;
+    J3 = -J;
+    profile = 1;
+elseif v0 < a0pow2_2J;
+    a_lim = sqrt(a0^2/2 - J*v0);
+    if a_lim < a0
+        a_lim = -sqrt(a0^2/2 + J*v0);
+        t1 = (a0 - a_lim)/J;
+        J1 = -J;
+        t2 = -a_lim/J;
+        J2 = J;
+        t3 = 0;
+        J3 = 0;
+    else
+        J1 = J;
+        t1 = (a_lim - a0)/J;
+        J2 = -J;
+        t2 = a_lim/J;
+        t3 = 0;
+        J3 = 0;
+    end
+    profile = 0;
+elseif v0 < -v_star
+    a_lim = -sqrt(a0^2/2 + J*v0);
+    t1 = (a0 - a_lim)/J;
     J1 = -J;
-    t2 = (a0 - a_lim)/J;
+    t2 = -a_lim/J;
     J2 = J;
     t3 = 0;
     J3 = 0;
     profile = 0;
-elseif v0 > v_star
-    t1 = d/J;
+else
+    t1 = (a0 + d)/J;
     J1 = -J;
-    t2 = (v0 - v_star)/d;
+    t2 = a0^2/(2*J*d) + a0/J + v0/d - t1;
     J2 = 0;
-    t3 = (d + a0)/J;
-    J3 = J;
-    profile = 1;
-
+    t3 = d/J;
+    J3 = +J;
+    profile = 1;    
 end
 
 if plot_traj
@@ -34,20 +77,12 @@ if plot_traj
     t1 = round(t1/dt)*dt;
     t2 = round(t2/dt)*dt;
     t3 = round(t3/dt)*dt;
-    if profile
-        t = 0:dt:1.05*(t1 + t2 + t3);
-        n = length(t);
-        nt1 = t1/dt - 1;
-        nt2 = t2/dt;
-        nt3 = t3/dt;
-        jt = [J1*ones(1,nt3) J2*ones(1,nt2) J3*ones(1,nt1) 0*ones(1,(n-nt1-nt2-nt3))];
-    else
-        t = 0:dt:1.05*(t1 + t2);
-        n = length(t);
-        nt1 = round(t1/dt - 1);
-        nt2 = round(t2/dt);   
-        jt = [J1*ones(1,nt2) J2*ones(1,nt1) 0*ones(1,(n-nt1-nt2))];
-    end
+    t = 0:dt:1.05*(t1 + t2 + t3);
+    n = length(t);
+    nt1 = int16(t1/dt);
+    nt2 = int16(t2/dt);
+    nt3 = int16(t3/dt);
+    jt = [J1*ones(1,nt1) J2*ones(1,nt2) J3*ones(1,nt3) 0*ones(1,(n-nt1-nt2-nt3))];
     at = zeros(1,n);
     vt = zeros(1,n);
     at(1) = a0;
